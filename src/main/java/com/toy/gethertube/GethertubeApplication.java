a4323eba33c544ffbe3f36c523812a4b2609be37
송형근 <soo1095@naver.com>
2024-07-16 00:15:38 +0900

    Spring Boot Project Init

---
+++ src/main/java/com/toy/gethertube/GethertubeApplication.java
@@ -0,0 +1,13 @@
+package com.toy.gethertube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GethertubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GethertubeApplication.class, args);
+    }
+
+}
