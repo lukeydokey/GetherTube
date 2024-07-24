9bd0ef21c96fc9f7502634e04ca7ed29f32560e7
KukJinKim0211 <rnrwls0211@naver.com>
2024-07-24 22:13:24 +0900

    Feat:Front: Youtube 라이브러리 추가, 라이브러리 테스트

--- front/gethertube/app/room/[id]/page.tsx
+++ front/gethertube/app/room/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import YouTube from "react-youtube";
 interface TypeParams {
   id: string;
 }
@@ -8,7 +10,31 @@ interface TypeRoomIdProps {
 
 const Page = ({ params }: TypeRoomIdProps) => {
   const { id } = params;
-  return <>{id}번방</>;
+  return (
+    <>
+      {id}번방
+      <div>
+        <YouTube
+          videoId="GZKj-PRPc2c" // defaults -> ''
+          id=""
+          className=""
+          iframeClassName=""
+          style={{}}
+          title=""
+          // loading={string} // defaults -> undefined
+          opts={{}}
+          onReady={() => {}}
+          onPlay={() => {}}
+          onPause={() => {}}
+          onEnd={() => {}}
+          onError={() => {}}
+          onStateChange={() => {}}
+          onPlaybackRateChange={() => {}}
+          onPlaybackQualityChange={() => {}}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Page;
