7450c92b569c960996d63fa96092b1be72f56ff4
KukJinKim0211 <rnrwls0211@naver.com>
2024-08-06 23:53:32 +0900

    Feat:Front: LocalStorage client side 처리

--- front/gethertube/util/utilStorage.ts
+++ front/gethertube/util/utilStorage.ts
@@ -3,16 +3,19 @@ type TypeStorageKey = "user" | "accessToken";
 
 export const utilStorage = () => {
   const getItem = (key: TypeStorageKey) => {
+    if (typeof window === "undefined") return "";
     if (!localStorage.getItem(key)) return "";
     return JSON.parse(localStorage.getItem(key) || "");
   };
 
   const setItem = (key: TypeStorageKey, value: string | object) => {
+    if (typeof window === "undefined") return "";
     if (typeof value === "string") localStorage.setItem(key, value);
     else localStorage.setItem(key, JSON.stringify(value));
   };
 
   const deleteItem = (key: TypeStorageKey) => {
+    if (typeof window === "undefined") return "";
     localStorage.removeItem(key);
   };
 
